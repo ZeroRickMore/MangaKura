c21d251d4b07d3e164a8d31ef45028be2e39ab0b
ZeroRickMore <riccardo.moretti.gg@gmail.com>
2025-02-27 00:41:39 +0100

    Visual refactor + Insert Wishlist URLs

--- MangaKura/GeneralHandler/urls.py
+++ MangaKura/GeneralHandler/urls.py
@@ -3,21 +3,35 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('search/<str:category>/', views.search_view, name='search'),
+
+    # ============================== MANGA STUFF ==============================
+
     path('insertManga/', views.insert_manga, name='insert_manga'),
-    path('insertVariant/', views.insert_variant, name='insert_variant'),
-    path('variant_detail/<int:variant_id>/', views.variant_detail, name='variant_detail'),
     path('manga_detail/<int:manga_id>/', views.manga_detail, name='manga_detail'),
     path('viewManga/', views.view_manga, name='view_manga'),
-    path('viewVariant/', views.view_variant, name='view_variant'),
     path('manga/<int:manga_id>/edit/', views.edit_manga, name='edit_manga'),
     path('manga/<int:manga_id>/delete/', views.delete_manga, name='delete_manga'),
+    path('viewManga/sortBy/', views.view_mangas, name='view_mangas'),
+
+    # ============================== VARIANT STUFF ==============================
+
+    path('insertVariant/', views.insert_variant, name='insert_variant'),
+    path('variant_detail/<int:variant_id>/', views.variant_detail, name='variant_detail'),
+    path('viewVariant/', views.view_variant, name='view_variant'),
     path('variant/<int:variant_id>/edit/', views.edit_variant, name='edit_variant'),
     path('variant/<int:variant_id>/delete/', views.delete_variant, name='delete_variant'),
-    path('search/<str:category>/', views.search_view, name='search'),
     path('viewVariant/sortBy/', views.view_variants, name='view_variants'),
-    path('viewManga/sortBy/', views.view_mangas, name='view_mangas'),
-    path('wishlist', views.view_wishlist, name='view_wishlist'),
     path('media/variant_images/<str:image_path>/', views.serve_protected_variant_image, name='serve_protected_variant_image'),
+
+    # ============================== WISHLIST STUFF ==============================
+
+    path('insertWishlistItem/', views.insert_wishlist_item, name='insert_wishlist_item'),
+    path('wishlist_item_detail/<int:wishlist_item_id>/', views.wishlist_item_detail, name='wishlist_item_detail'),
+    path('wishlist', views.view_wishlist, name='view_wishlist'),
+    path('wishlist_item/<int:wishlist_item_id>/edit/', views.edit_wishlist_item, name='edit_wishlist_item'),
+    path('wishlist_item/<int:wishlist_item_id>/delete/', views.delete_wishlist_item, name='delete_wishlist_item'),
+    path('media/wishlist_images/<str:image_path>/', views.serve_protected_wishlist_item_image, name='serve_protected_wishlist_item_image'),
 ]
 
 
