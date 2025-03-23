5ba20704e618859ef327937e4fc63eeec7c8a5a4
ZeroRickMore <riccardo.moretti.gg@gmail.com>
2025-03-23 12:10:29 +0100

    Create UserToExtraInfos object upon registration

--- MangaKura/MangaKura/views.py
+++ MangaKura/MangaKura/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm
+from GeneralHandler.models import UserToExtraInfos
+
+# TEST USER: test1, testpwd12!
 
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
+
+            UserToExtraInfos.objects.create(user=user) # Create entry for the user
+
             login(request, user)
             return redirect('home')
     else:
