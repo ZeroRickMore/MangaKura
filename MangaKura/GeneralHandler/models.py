9574a44e71895c86c41247e7ad1144bcd22af2bb
ZeroRickMore <riccardo.moretti.gg@gmail.com>
2025-03-23 12:09:28 +0100

    Do not delete everything on user deletion

--- MangaKura/GeneralHandler/models.py
+++ MangaKura/GeneralHandler/models.py
@@ -4,18 +4,19 @@ from django.contrib.auth.models import User
 # This models links a manga to a user. Not necessary to have a "Manga" object of its own as this is strictly related to the user.
 # Like, a user does not link a manga from a general database, he simply "creates" a new manga each time, just make sure that it exists because else it's a useless service!
 class UserToManga(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    manga_title = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    manga_title = models.CharField(max_length=100)
     animeclick_url = models.URLField(blank=True, null=True)
     single_volume_price = models.FloatField(default=0.0)
     whole_series_price = models.FloatField(default=0.0, blank=True)
-    owned_volumes = models.CharField(max_length=255)
-    physical_position = models.CharField(max_length=255)
-    volume_doubles = models.CharField(max_length=255, blank=True, null=True)
+    owned_volumes = models.CharField(max_length=100)
+    physical_position = models.CharField(max_length=100)
+    volume_doubles = models.CharField(max_length=100, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     all_read = models.BooleanField(default=False, blank=True)
     completed = models.BooleanField(default=False, blank=True)
     all_published = models.BooleanField(default=False, blank=True)
+    editor = models.CharField(max_length=30, null=True, blank=True)
     #rating = models.FloatField(deafult=0.0, blank=True)
     whole_series_price_calculated = models.FloatField(default=0.0, blank=True) # The user has to way to interact with this.
 
@@ -28,13 +29,13 @@ class UserToManga(models.Model):
 # Like, a user does not link a Variant from a general database, he simply "creates" a new Variant each time, just make sure that it exists because else it's a useless service!
 
 class UserToVariant(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    variant_title = models.CharField(max_length=255)
-    related_manga_title = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    variant_title = models.CharField(max_length=100)
+    related_manga_title = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     stock_price = models.FloatField(blank=True, null=True)
     current_selling_price = models.FloatField(blank=True, null=True)
-    physical_position = models.CharField(max_length=255)
+    physical_position = models.CharField(max_length=100)
     number_of_owned_copies = models.IntegerField()
     copies_sold = models.JSONField(default=list)
     useful_links = models.JSONField(default=list)
@@ -59,8 +60,8 @@ class VariantImage(models.Model):
 # Something that a user wants to buy, to put in a Wishlist.""
 # Please note that the "Wishlist" itself is not a real Object, rather it is obtained by querying for UserToWishlist where user=logged_user
 class UserToWishlistItem(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    title = models.CharField(max_length=100)
     price = models.FloatField(blank=True, null=True)
     release_date = models.DateTimeField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
@@ -79,4 +80,12 @@ class WishlistImage(models.Model):
     def __str__(self):
         return f"Image for {self.wishlist_item.title}"
     
-    
+
+
+class UserToExtraInfos(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    MANGA_STATS_TO_BE_MODIFIED = models.BooleanField(default=True)
+    manga_stats = models.JSONField(null=True, blank=True)
+
+    def __str__(self):
+        return f"-USER : [{self.user}] \n- MANGA_STATS_TO_BE_MODIFIED : [{self.MANGA_STATS_TO_BE_MODIFIED}] \n- manga_stats : [{self.manga_stats}] \n"
