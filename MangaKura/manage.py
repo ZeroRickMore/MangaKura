893fcd8e91731bb17f298f11effbf3f81e072ce2
ZeroRickMore <riccardo.moretti.gg@gmail.com>
2025-03-23 12:09:58 +0100

    Quickstart

--- MangaKura/manage.py
+++ MangaKura/manage.py
@@ -3,7 +3,6 @@
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MangaKura.settings')
@@ -15,7 +14,17 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    
+    # When executing from mobile, I simply run this script with no args.
+    # That being said, we consider the scenario where if len(sys.argv) == 1, we are on mobile, straight up.
+    # So, we go into LAZY mode, which is a mode that skips heavy calculations like the manga stats, to be mobile-friendly.
+
+    if len(sys.argv) == 1:
+        # os.environ["IS_MOBILE"] = "1" # TODO Do I still need it?
+        sys.argv.append('runserver')
+
     execute_from_command_line(sys.argv)
+
 
 
 if __name__ == '__main__':
